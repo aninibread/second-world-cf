4b2fffc9b4faab685dee8d8e4cad085b48abf992
Anni Wang <anniwang44@gmail.com>
2024-02-20 16:35:24 -0500

    add back video

--- src/pages/cat.js
+++ src/pages/cat.js
@@ -46,10 +46,17 @@ export default function Cat() {
       <p>This is my kitty cat. Her name is Jiajia and she is turning 3 y/o (2024). Her name Jiajia or 佳佳 is a common Chinese name character that shares the same sound with the word 家 meaning 'Home'. </p>
       <br/>
         <div className={galleryStyles.masonryGrid}>
-            {catImageKeys && catImageKeys.map((imageKey, index) => (
+              {catImageKeys && catImageKeys.map((imageKey, index) => (
                 <div key={index} className={galleryStyles.masonryGridItem}>
                     <figure>
+                      {imageKey.endsWith("mp4") ? (
+                          <video autoPlay loop muted className="w-full h-auto rounded-lg shadow-lg">
+                              <source src={getImageHostUrl(imageKey)} type="video/mp4" />
+                              Your browser does not support the video tag.
+                          </video>
+                      ):
                         <img src={getImageHostUrl(imageKey)} alt={formatTitle(imageKey)} className="w-full h-auto rounded-lg shadow-lg" />
+                      }
                     </figure>
                 </div>
             ))}
