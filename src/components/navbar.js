c4f6afc449a837f5568aa3d4ac9fbabb1d643165
Anni Wang <anniwang44@gmail.com>
2024-02-23 22:07:14 -0500

    spinning logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,9 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <header className="flex w-full max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 justify-between items-center py-6">
-      <div className="logo text-5xl">🧸</div>
+      <div className="logo">
+        <img src="https://pub-4b3c8e02204249afb15ca13b88ec64ef.r2.dev/nav-logo.png" alt="Logo" className="h-12 w-12 spin " />
+      </div>
       <nav className="nav space-x-4">
         <Link href="/" title="info">info</Link>
         <Link href="/writing" title="writing">writing</Link>
